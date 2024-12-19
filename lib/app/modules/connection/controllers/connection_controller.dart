8264442a31cff8b93a1048007e50f54ad0620ec5
Farriel Arrianta <farrielarrianta@gmail.com>
2024-12-19 11:47:45 +0700

    fixed connection_controller to go back to last route if internet connected again

--- lib/app/modules/connection/controllers/connection_controller.dart
+++ lib/app/modules/connection/controllers/connection_controller.dart
@@ -1,36 +1,47 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:get/get.dart';
-import '../../login/views/login_view.dart';
 import '../views/no_connection_view.dart';
 
 class ConnectionController extends GetxController {
   final Connectivity _connectivity = Connectivity();
+  // Store the last route before losing connection
+  String? _lastRoute;
+  Map<String, dynamic>? _lastArguments;
 
   @override
   void onInit() {
     super.onInit();
-    _connectivity.onConnectivityChanged.listen((connectivityResults) {
-      _updateConnectionStatus(connectivityResults.first);
+    _connectivity.onConnectivityChanged.listen((results) {
+      _updateConnectionStatus(results.first);
     });
   }
 
   void _updateConnectionStatus(ConnectivityResult connectivityResult) {
-    // Cek apakah saat ini sedang di halaman login
-    bool isLoginPage = Get.currentRoute == '/login';
-
-    if (connectivityResult == ConnectivityResult.none && isLoginPage || Get.currentRoute == '/explore') {
-      // Hanya beralih ke halaman no connection jika sedang di halaman login
-      Get.offAll(() => const NoConnectionView());
+    if (connectivityResult == ConnectivityResult.none) {
+      // Store current route and arguments before navigating to no connection view
+      if (Get.currentRoute != '/NoConnectionView') {
+        _lastRoute = Get.currentRoute;
+        _lastArguments = Get.arguments;
+        Get.offAll(() => const NoConnectionView());
+      }
     } else if (connectivityResult != ConnectivityResult.none &&
         Get.currentRoute == '/NoConnectionView') {
-      // Kembali ke halaman login jika sudah terhubung kembali
-      Get.offAll(() => const LoginView());
+      // Return to the last route if it exists, otherwise go to home
+      if (_lastRoute != null) {
+        Get.offAllNamed(_lastRoute!, arguments: _lastArguments);
+        // Clear stored route after navigation
+        _lastRoute = null;
+        _lastArguments = null;
+      }
     }
   }
 
-  // Metode untuk pengecekan koneksi manual
+  // Method for manual connection check
   Future<bool> checkInternetConnection() async {
     var connectivityResult = await _connectivity.checkConnectivity();
     return connectivityResult != ConnectivityResult.none;
   }
+
+  // Method to get the last route before connection loss
+  String? getLastRoute() => _lastRoute;
 }
